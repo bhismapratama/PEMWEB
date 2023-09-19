b8ea1963a8703f61e278a7cb02c3a2fa2383dee4
bhismapratama <bhismaelki@gmail.com>
2023-09-19 11:51:33 +0700

    feat: minggu 3 about JavaScript add all clear

--- js.js
+++ js.js
@@ -4,6 +4,9 @@ const buttonBagi = document.getElementById('btnbagi');
 const buttonKali = document.getElementById('btnkali');
 const buttonMod = document.getElementById('btnmod');
 
+const buttonAll = document.getElementById('btnall');
+const buttonClear = document.getElementById('btnclear');
+
 const tambah = document.getElementById('tambah')
 const kurang = document.getElementById('kurang')
 const bagi = document.getElementById('bagi')
@@ -12,76 +15,61 @@ const kali = document.getElementById('kali')
 buttonTambah.addEventListener('click', () => {
     let datas = Number(document.getElementById('input1').value);
     let datas2 = Number(document.getElementById('input2').value);
-    
     let checkTambah = datas+datas2
-    // let checkKurang = datas-datas2
-    // let checkBagi = datas/datas2
-    // let checkKali = datas*datas2
 
     tambah.innerHTML = "ini tambah = " + checkTambah;
-    // kurang.innerHTML = "ini kurang = " + checkKurang;
-    // bagi.innerHTML = "ini bagi = " + checkBagi;
-    // kali.innerHTML = "ini kali = " + checkKali;
 })
 
 buttonKurang.addEventListener('click', () => {
     let datas = Number(document.getElementById('input1').value);
     let datas2 = Number(document.getElementById('input2').value);
-    
-    // let checkTambah = datas+datas2
     let checkKurang = datas-datas2
-    // let checkBagi = datas/datas2
-    // let checkKali = datas*datas2
-
-    // tambah.innerHTML = "ini tambah = " + checkTambah;
     kurang.innerHTML = "ini kurang = " + checkKurang;
-    // bagi.innerHTML = "ini bagi = " + checkBagi;
-    // kali.innerHTML = "ini kali = " + checkKali;
 })
 
 buttonBagi.addEventListener('click', () => {
     let datas = Number(document.getElementById('input1').value);
     let datas2 = Number(document.getElementById('input2').value);
-    
-    // let checkTambah = datas+datas2
-    // let checkKurang = datas-datas2
     let checkBagi = datas/datas2
-    // let checkKali = datas*datas2
-
-    // tambah.innerHTML = "ini tambah = " + checkTambah;
-    // kurang.innerHTML = "ini kurang = " + checkKurang;
     bagi.innerHTML = "ini bagi = " + checkBagi;
-    // kali.innerHTML = "ini kali = " + checkKali;
 })
 
 buttonKali.addEventListener('click', () => {
     let datas = Number(document.getElementById('input1').value);
     let datas2 = Number(document.getElementById('input2').value);
-    
-    // let checkTambah = datas+datas2
-    // let checkKurang = datas-datas2
-    // let checkBagi = datas/datas2
     let checkKali = datas*datas2
-
-    // tambah.innerHTML = "ini tambah = " + checkTambah;
-    // kurang.innerHTML = "ini kurang = " + checkKurang;
-    // bagi.innerHTML = "ini bagi = " + checkBagi;
     kali.innerHTML = "ini kali = " + checkKali;
 })
 
 buttonMod.addEventListener('click', () => {
     let datas = Number(document.getElementById('input1').value);
     let datas2 = Number(document.getElementById('input2').value);
+    let checkMod = datas%datas2
+    mod.innerHTML = "ini modulus = " + checkMod;
+})
+
+
+buttonAll.addEventListener('click', () => {
+    let datas = Number(document.getElementById('input1').value);
+    let datas2 = Number(document.getElementById('input2').value);
     
-    // let checkTambah = datas+datas2
-    // let checkKurang = datas-datas2
-    // let checkBagi = datas/datas2
-    // let checkKali = datas*datas2
+    let checkTambah = datas+datas2
+    let checkKurang = datas-datas2
+    let checkBagi = datas/datas2
+    let checkKali = datas*datas2
     let checkMod = datas%datas2
 
-    // tambah.innerHTML = "ini tambah = " + checkTambah;
-    // kurang.innerHTML = "ini kurang = " + checkKurang;
-    // bagi.innerHTML = "ini bagi = " + checkBagi;
-    // kali.innerHTML = "ini kali = " + checkKali;
+    tambah.innerHTML = "ini tambah = " + checkTambah;
+    kurang.innerHTML = "ini kurang = " + checkKurang;
+    bagi.innerHTML = "ini bagi = " + checkBagi;
+    kali.innerHTML = "ini kali = " + checkKali;
     mod.innerHTML = "ini modulus = " + checkMod;
+})
+
+buttonClear.addEventListener('click', () => {
+    tambah.innerHTML = "ini tambah = terhapus yak";
+    kurang.innerHTML = "ini kurang = terhapus yak";
+    bagi.innerHTML = "ini bagi = terhapus yak";
+    kali.innerHTML = "ini kali = terhapus yak";
+    mod.innerHTML = "ini modulus = terhapus yak";
 })
